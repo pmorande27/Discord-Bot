f0a4d27478b380449c76bf93dd21bed023d9fef9
root <root@DESKTOP-B750EJ2.localdomain>
2021-04-01 17:37:32 +0200

    Add latex transformation ability

--- bot.py
+++ bot.py
@@ -1,25 +1,42 @@
 import discord
 from discord.ext import commands
-
+import sympy
+from sympy import symbols, Symbol,preview
+import math
 client = commands.Bot(command_prefix = "!")
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-@client.event
-async def on_joim
 @client.command()
 async def ping(ctx):
     await ctx.send("pong")
-@cliend.command()
+@client.command()
+@commands.is_owner()
 async def kick(ctx, member : discord.Member,*, reason = None):
     await member.kick(reason = reason)
-@cliend.command()
+
+@client.command()
+@commands.is_owner()
 async def ban(ctx, member : discord.Member,*, reason = None):
     await member.ban(reason = reason)
+
+@client.command()
+@commands.is_owner()
+async def unban(ctx,*, member):
+    banned_users = await ctx.guild.bans()
+    member_name,member_discriminator = member.split('#')
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        if(user.name,user.discriminator) == (member_name,member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f"Unbanned {user.mention}")
+            return
+
+@client.command()
+async def latex(ctx,*,latex):
+    preview(latex, viewer='file', filename='a.png', euler=False)
+    await ctx.send(file=discord.File('a.png'))
     
-
-
-
 
 
 client.run(token)
